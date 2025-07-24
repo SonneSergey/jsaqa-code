cac7d88fc2beb6191eb3905c455c27b7e46bc49c
Sergey <priestsitnikovsergey@gmail.com>
2025-07-24 22:04:17 +0300

    Создан файл .gitignore в папке с добавлением user.js, настроена конфигурация package.json, добавлены и обновлены тесты

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,22 +1,44 @@
-const { test, expect } = require("@playwright/test");
+import { test, expect } from "@playwright/test";
+import { EMAIL, PASSWORD } from "./user.js";
 
-test("test", async ({ page }) => {
-  // Go to https://netology.ru/free/management#/
-  await page.goto("https://netology.ru/free/management#/");
 
-  // Click a
-  await page.click("a");
-  await expect(page).toHaveURL("https://netology.ru/");
+test("Успешная авторизация", async ({ page }) => {
+  // 1. Страница авторизации
+  await page.goto("https://netology.ru/?modal=sign_in", { timeout: 60000 });
 
-  // Click text=Учиться бесплатно
-  await page.click("text=Учиться бесплатно");
-  await expect(page).toHaveURL("https://netology.ru/free");
+  // 2. Форма
+  await page.locator('input[name="email"][placeholder="Email"]').fill(EMAIL);
+  await page.locator('input[name="password"]').fill(PASSWORD);
 
-  page.click("text=Бизнес и управление");
+  // 3. Клик кнопки "Войти"
+  await page.locator('button[type="submit"]:has-text("Войти")').click();
 
-  // Click text=Как перенести своё дело в онлайн
-  await page.click("text=Как перенести своё дело в онлайн");
-  await expect(page).toHaveURL(
-    "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
+  // 4. Сообщение об успешном входе
+  // await expect(page.locator('text=Привет, Сергей! Вы вошли в аккаунт')).toBeVisible({ timeout: 30000 });
+
+  // 5. Проверка заголовка профиля
+  await expect(page.locator('h2:has-text("Моё обучение")')).toBeVisible({ timeout: 15000 });
+
+  // 6. Дополнительная проверка URL
+  await expect(page).toHaveURL(/profile/);
+});
+
+test("Неуспешная авторизация", async ({ page }) => {
+  // 1. Страница авторизации
+  await page.goto("https://netology.ru/?modal=sign_in", { timeout: 60000 });
+
+  // 2. Форма невалидными данными
+  await page.locator('input[name="email"][placeholder="Email"]').fill("wrong@example.com");
+  await page.locator('input[name="password"]').fill("wrongpassword");
+
+  // 3. Клик кнопки "Войти"
+  await page.locator('button[type="submit"]:has-text("Войти")').click();
+
+  // 4. Проверка на появление ошибки)
+  await expect(page.locator('[data-testid="login-error-hint"]')).toHaveText(
+    "Вы ввели неправильно логин или пароль.",
+    { timeout: 30000 }
   );
+
+  await expect(page).not.toHaveURL(/profile/);
 });
